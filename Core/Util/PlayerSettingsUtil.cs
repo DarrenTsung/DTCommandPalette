6ddfb9d3de392d22f8743606a10c3b75754acd52
Darren Tsung <darren.tsung@gmail.com>
2017-01-31 10:50:24 -0800

    Fix UnityEditor references not being in Editor folder or surrounded by #if UNITY_EDITOR

--- Core/Util/PlayerSettingsUtil.cs
+++ Core/Util/PlayerSettingsUtil.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -22,3 +23,4 @@ namespace DTCommandPalette {
         }
     }
 }
+#endif
--- Core/Util/ScriptableObjectEditorUtil.cs
+++ Core/Util/ScriptableObjectEditorUtil.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -25,3 +26,4 @@ namespace DTCommandPalette {
         }
     }
 }
+#endif
