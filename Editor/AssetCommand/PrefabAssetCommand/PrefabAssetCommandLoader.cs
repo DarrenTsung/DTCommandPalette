677a7f0af01b5d584d592e2c44c2690e5e89f0e9
Darren Tsung <darren.tsung@gmail.com>
2017-01-31 12:03:01 -0800

    Don’t show prefab commands if nothing is registered to handle them

--- Editor/AssetCommand/PrefabAssetCommand/PrefabAssetCommandLoader.cs
+++ Editor/AssetCommand/PrefabAssetCommand/PrefabAssetCommandLoader.cs
@@ -4,8 +4,14 @@ using UnityEditor;
 
 namespace DTCommandPalette {
     public class PrefabAssetCommandLoader : ICommandLoader {
+        private static readonly ICommand[] kEmptyArray = new ICommand[0];
+
         // PRAGMA MARK - ICommandLoader
         public ICommand[] Load() {
+            if (PrefabAssetCommand.OnPrefabGUIDExecuted == null) {
+                return kEmptyArray;
+            }
+
             string[] guids = AssetDatabase.FindAssets("t:Prefab");
 
             List<ICommand> objects = new List<ICommand>();
